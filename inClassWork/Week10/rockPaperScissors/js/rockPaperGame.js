ab59cf27e76b67e8ad73eff341564bd485ee3fe6
chrismald0228 <149415670+chrismald0228@users.noreply.github.com>
2024-11-06 13:31:50 -0600

    updated to display images for results

--- inClassWork/Week10/rockPaperScissors/js/rockPaperGame.js
+++ inClassWork/Week10/rockPaperScissors/js/rockPaperGame.js
@@ -3,11 +3,16 @@ const ui = {
         const imgHtml = `<img src='imgs/${pickObj.img}' width='100px' alt="${pickObj.item}"'>`;
         document.getElementById(disId).innerHTML = imgHtml;
     },
-    updateDisplayValue: function(value, disId) {
-        document.getElementById(disId).innerText = value;
+    updateDisplayObj: function(msgObj, disId) {
+        let inHTML  = msgObj.msg;
+        inHTML += `<img src='imgs/${msgObj.img}' width='100px' alt="${msgObj.alt}"'>`;
+        // alert( inHTML );
+        document.getElementById(disId).innerHTML = inHTML;
     },
-    updateResultValue: function(value, disId) {
-
+    updateDisplayValue: function(msg, disId) {
+        // let inHTML  = msgObj.msg;
+        // inHTML += `<br /><img src='imgs/${msgObj.img}' width='100px' alt="${msgObj.alt}"'>`;
+        document.getElementById(disId).innerHTML = msg;
     },
     updateStatsDisplay: function(state) {
         this.updateDisplayValue(state.wins, "wins");
@@ -28,9 +33,9 @@ const game = {
         draws: 0
     },
     messages: {
-        win: "Winner!",
-        loss: "Loser!",
-        draw: "Draw!",
+        win: {msg : "Winner!", img : 'homerWin.jpg', alt:"Homer Wins!"},
+        loss: {msg : "Loser!", img: 'homerLoss.png', alt:"Homer Loss!"},
+        draw: {msg : "Draw!", img: 'homerTied.png', alt : "Homer Tied up!"}
     },
     getRandomInt: function(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -38,12 +43,15 @@ const game = {
     evaluateResult: function(cPickObj, uPickObj) {
         if (uPickObj.beats === cPickObj.item) {
             this.state.wins++;
+            // alert( "Win");
             return this.messages.win;
         } else if (cPickObj.beats === uPickObj.item) {
             this.state.losses++;
+            // alert( "Loss");
             return this.messages.loss;
         } else {
             this.state.draws++;
+            // alert( "Draw");
             return this.messages.draw;
         }
     }
@@ -59,8 +67,9 @@ document.addEventListener("DOMContentLoaded", function() {
         const uPickObj = game.rules[uPickIndex];
         ui.displayImage(uPickObj, "uPick");
 
-        const resultMessage = game.evaluateResult(cPickObj, uPickObj);
-        ui.updateDisplayValue(resultMessage, "resArea");
+        const resultObj = game.evaluateResult(cPickObj, uPickObj);
+        // document.getElementById("resArea").innerHTML = resultMessage;
+        ui.updateDisplayObj(resultObj, "resArea");
         ui.updateStatsDisplay(game.state); // Call to update all stats
     });
 });
