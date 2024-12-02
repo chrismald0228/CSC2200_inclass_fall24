b1663f97f952bff0f9c5b9d1208a9de8d1549b54
chrismald0228 <149415670+chrismald0228@users.noreply.github.com>
2024-12-02 14:12:21 -0600

    worked with JSON

---
+++ inClassWork/Week 15/menu.js
@@ -0,0 +1,17 @@
+const menu = {
+    menuItems: [
+        {
+            name: "Burger",
+            price: 5.99,
+        },
+        {
+            name: "Pizza",
+            price: 8.99,
+        },
+        {
+            name: "Salad",
+            price: 4.99,
+
+        }
+    ]
+};
